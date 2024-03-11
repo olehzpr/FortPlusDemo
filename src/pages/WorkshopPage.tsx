2ce1e7118ed68b1020f7f62275b91b5cf759d797
Oleg Zapara <ole9zp@gmail.com>
2024-03-11 19:52:32 +0200

    Gas workshop layout update

--- src/pages/WorkshopPage.tsx
+++ src/pages/WorkshopPage.tsx
@@ -1,30 +1,91 @@
-import data from '../assets/WorkshopData.json'
-import ScrollArrow from '../components/ScrollArrow';
-import "./WorkshopPage.css"
+import data from "../assets/WorkshopData.json";
+import ScrollArrow from "../components/ScrollArrow";
+import "./WorkshopPage.css";
 
 export default function WorkshopPage() {
-  const rows = data.rows;
-
+  const equipment = data.equipment;
+  const service = data.service;
+  const testing = data.testing;
+  const repair = data.repair;
   return (
-    <div className='WorkshopPage'>
+    <div className="WorkshopPage">
       <ScrollArrow></ScrollArrow>
-      <div className='page-content'>
-        <div className='header'>
-          Газова майстерня
+      <div className="page-content">
+        <div className="header">
+          Встановлення газобалонного обладнання на автомобілі
         </div>
-        <ul className='responsive-table'>
-          <li className='table-header'>
-            <div className='col col-1'>Позиція</div>
-            <div className='col col-2'>Найменування послуги</div>
-          </li>    
-          {rows.map((x, i) => (
-            <li key={i} className='table-row'>
-              <div className='col col-1' data-label="Номер:">{i + 1}</div>
-              <div className='col col-2' data-label="Назва:">{x}</div>
-            </li>    
+        <ul className="responsive-table">
+          <li className="table-header">
+            <div className="col col-1">Позиція</div>
+            <div className="col col-2">Найменування послуги</div>
+          </li>
+          {equipment.map((x, i) => (
+            <li key={i} className="table-row">
+              <div className="col col-1" data-label="Номер:">
+                {i + 1}
+              </div>
+              <div className="col col-2" data-label="Назва:">
+                {x}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="header">
+          Технічне обслуговування, ремонт газобалонного обладнання
+        </div>
+        <ul className="responsive-table">
+          <li className="table-header">
+            <div className="col col-1">Позиція</div>
+            <div className="col col-2">Найменування послуги</div>
+          </li>
+          {service.map((x, i) => (
+            <li key={i} className="table-row">
+              <div className="col col-1" data-label="Номер:">
+                {i + 1}
+              </div>
+              <div className="col col-2" data-label="Назва:">
+                {x}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="header">
+          Випробування газових балонів, наливних рукавів, клапанів
+        </div>
+        <ul className="responsive-table">
+          <li className="table-header">
+            <div className="col col-1">Позиція</div>
+            <div className="col col-2">Найменування послуги</div>
+          </li>
+          {testing.map((x, i) => (
+            <li key={i} className="table-row">
+              <div className="col col-1" data-label="Номер:">
+                {i + 1}
+              </div>
+              <div className="col col-2" data-label="Назва:">
+                {x}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="header">ТО та Ремонт автомобілів</div>
+        <ul className="responsive-table">
+          <li className="table-header">
+            <div className="col col-1">Позиція</div>
+            <div className="col col-2">Найменування послуги</div>
+          </li>
+          {repair.map((x, i) => (
+            <li key={i} className="table-row">
+              <div className="col col-1" data-label="Номер:">
+                {i + 1}
+              </div>
+              <div className="col col-2" data-label="Назва:">
+                {x}
+              </div>
+            </li>
           ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
